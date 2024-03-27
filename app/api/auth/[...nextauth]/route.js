be65256cc4ac20b463f5201e50fad3a908ff445e
Kunal Borkar <137871453+kunalborkar2001@users.noreply.github.com>
2024-03-27 11:48:27 +0530

    added secret

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -11,6 +11,7 @@ const handler = NextAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         })
     ],
+    secret: process.env.NEXTAUTH_URL_SECRET,
     callbacks: {
         async session({ session }) {
             // store the user id from MongoDB to session
