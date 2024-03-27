f32087ba15d10a3a6134caae2f71216ba50e1190
Kunal Borkar <137871453+kunalborkar2001@users.noreply.github.com>
2024-03-27 12:04:20 +0530

    home page post populate

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,14 +1,22 @@
-import Prompt from "@models/prompt";
-import { connectToDB } from "@utils/database";
+import Prompt from '@models/prompt';
+import { connectToDB } from '@utils/database';
+import { headers } from 'next/headers';
 
 export const GET = async (request) => {
+    const headersList = headers();
+    const referer = headersList.get('referer');
     try {
-        await connectToDB()
+        await connectToDB();
 
-        const prompts = await Prompt.find({}).populate('creator')
-
-        return new Response(JSON.stringify(prompts), { status: 200 })
+        const prompts = await Prompt.find({}).populate('creator');
+        return new Response(JSON.stringify(prompts), {
+            status: 200,
+            headers: {
+                referer: referer,
+                'Cache-Control': 'no-cache, no-store, max-age=0, must-revalidate',
+            },
+        });
     } catch (error) {
-        return new Response("Failed to fetch all prompts", { status: 500 })
+        return new Response('Failed to fetch all prompts', { status: 500 });
     }
-} 
+};
